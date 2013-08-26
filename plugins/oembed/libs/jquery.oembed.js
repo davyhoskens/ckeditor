defb44b0d87b20ca4aac773d998db0817e7e295a
Eymen Gunay <eymen@egunay.com>
2013-08-26 16:52:25 +0300

    Oembed plugin updated

--- plugins/oembed/libs/jquery.oembed.js
+++ plugins/oembed/libs/jquery.oembed.js
@@ -300,7 +300,6 @@
               }
               
             }
-
             var code = $('<'+tag+'/>')
             .attr('src',src)
             .attr('width',width)
@@ -316,8 +315,7 @@
           if(tag=='iframe')
             code
               .attr('scrolling',embedProvider.embedtag.scrolling || "no")
-              .attr('frameborder',embedProvider.embedtag.frameborder || "0");
-            
+              .attr('frameborder',embedProvider.embedtag.frameborder || "0");            
             
           var oembedData = {code: code};
           success(oembedData, externalUrl,container);
@@ -438,11 +436,18 @@
         }
     };
 
-    $.fn.oembed.getPhotoCode = function(url, oembedData) {
+    $.fn.oembed.getPhotoCode = function (url, oembedData) {
         var code, alt = oembedData.title ? oembedData.title : '';
         alt += oembedData.author_name ? ' - ' + oembedData.author_name : '';
         alt += oembedData.provider_name ? ' - ' + oembedData.provider_name : '';
-        code = '<div><a href="' + url + '" target=\'_blank\'><img src="' + oembedData.url + '" alt="' + alt + '"/></a></div>';
+        if (oembedData.url) {
+            code = '<div><a href="' + url + '" target=\'_blank\'><img src="' + oembedData.url + '" alt="' + alt + '"/></a></div>';
+        } else if (oembedData.thumbnail_url) {
+            var newURL = oembedData.thumbnail_url.replace('_s', '_b');
+            code = '<div><a href="' + url + '" target=\'_blank\'><img src="' + newURL + '" alt="' + alt + '"/></a></div>';
+        } else {
+            code = '<div>Error loading this picture</div>';
+        }
         if (oembedData.html) code += "<div>" + oembedData.html + "</div>";
         return code;
     };
@@ -495,6 +500,12 @@
                   if (results.json.type != 'video' && (results.json.url || results.json.thumbnail_url)) {
 						return '<img src="' + (results.json.url || results.json.thumbnail_url) + '"  />';
                   } else if (results.json.html.indexOf("iframe")) {
+
+                      // Quick fix to handle attribute less html5 properties in ckeditor
+                      if (results.json.html.indexOf("allowfullscreen>")) {
+                          results.json.html = results.json.html.replace('allowfullscreen>', 'allowfullscreen="false">');
+                      }
+                      
                       var html = $.parseHTML(results.json.html);
 
                       var width = html[0].width;
@@ -560,6 +571,39 @@
         this.embedtag = this.embedtag || {tag:""};
 
         
+    };
+    
+    /*
+     * Function to update existing providers
+     *
+     * @param  {String}    name             The name of the provider
+     * @param  {String}    type             The type of the provider can be "file", "photo", "video", "rich"
+     * @param  {String}    urlshemesarray   Array of url of the provider
+     * @param  {String}    apiendpoint      The endpoint of the provider
+     * @param  {String}    extraSettings    Extra settings of the provider
+     */
+    $.fn.updateOEmbedProvider = function (name, type, urlschemesarray, apiendpoint, extraSettings) {
+        for (var i = 0; i < $.fn.oembed.providers.length; i++) {
+            if ($.fn.oembed.providers[i].name === name) {
+                if (type !== null) {
+                    $.fn.oembed.providers[i].type = type;
+                }
+                if (urlschemesarray !== null) {
+                    $.fn.oembed.providers[i].urlschemes = urlschemesarray;
+                }
+                if (apiendpoint !== null) {
+                    $.fn.oembed.providers[i].apiendpoint = apiendpoint;
+                }
+                if (extraSettings !== null) {
+                    $.fn.oembed.providers[i].extraSettings = extraSettings;
+                    for (var property in extraSettings) {
+                        if (extraSettings[property] !== null) {
+                            $.fn.oembed.providers[i][property] = extraSettings[property];
+                        }
+                    }
+                }
+            }
+        }
     };
 
     /* Native & common providers */
@@ -634,7 +678,7 @@
     new $.fn.oembed.OEmbedProvider("official.fm", "rich", ["official.fm/.+"], 'http://official.fm/services/oembed',{useYQL:'json'}),
     new $.fn.oembed.OEmbedProvider("chirbit", "rich", ["chirb.it/.+"], 'http://chirb.it/oembed.json',{useYQL:'json'}),
     new $.fn.oembed.OEmbedProvider("Huffduffer", "rich", ["huffduffer.com/[-.\\w@]+/\\d+"], "http://huffduffer.com/oembed"),
-    new $.fn.oembed.OEmbedProvider("Spotify", "rich", ["open.spotify.com/(track|album)/"], "https://embed.spotify.com/oembed/"),
+    new $.fn.oembed.OEmbedProvider("Spotify", "rich", ["open.spotify.com/(track|album|user)/"], "https://embed.spotify.com/oembed/"),
     new $.fn.oembed.OEmbedProvider("shoudio", "rich", ["shoudio.com/.+","shoud.io/.+"], "http://shoudio.com/api/oembed"),
     new $.fn.oembed.OEmbedProvider("mixcloud", "rich", ["mixcloud.com/.+"], checkProtocol() + 'www.mixcloud.com/oembed/', { useYQL: 'json' }),
     new $.fn.oembed.OEmbedProvider("rdio.com", "rich", ["rd.io/.+", "rdio.com"], checkProtocol() + "www.rdio.com/api/oembed/"),
